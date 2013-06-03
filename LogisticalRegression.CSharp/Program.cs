4b58b870db13c790351261edeac3b7030910e807
Scott Lerch <scottlerch@gmail.com>
2013-06-03 00:21:21 -0700

    Add skeleton for logistical regression

--- LogisticalRegression.CSharp/Program.cs
+++ LogisticalRegression.CSharp/Program.cs
@@ -1,15 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LogisticalRegression.CSharp
+﻿namespace LogisticalRegression
 {
+    using System;
+    using System.IO;
+    using System.Linq;
+    using MathNet.Numerics.LinearAlgebra.Double;
+    using MathNet.Numerics.LinearAlgebra.Generic;
+    using MathNet.Numerics.Statistics;
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Loading data...");
+
+            var data = (from line in File.ReadLines("ex2data1.txt")
+                        let items = line.Split(',')
+                        select items.Select(double.Parse).ToList()).ToList();
+
+            var sourceX = DenseMatrix.Create(data.Count, data.First().Count - 1, (i, j) => data[i][j]);
+            var y = DenseVector.Create(data.Count, i => data[i].Last());
+
+            // TODO:
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        private static double Sigmoid(double value)
+        {
+            return 1.0 / (1.0 - Math.Exp(-value));
+        }
+
+        private static Tuple<double, DenseVector> RegularizedLogisticalRegressionCostFunction(
+            DenseVector theta, DenseMatrix X, DenseVector y, double lamda)
+        {
+            int m = y.Count;
+            double J = 0.0;
+            DenseVector grad = DenseVector.Create(theta.Count, i => 0.0);
+
+            // TODO:
+
+            return Tuple.Create(J, grad);
+        }
+
+        private DenseVector Predict(Vector<double> theta, Matrix<double> X)
+        {
+            var temp = (theta * X.Transpose());
+            
+            throw new NotImplementedException();
         }
     }
 }
--- LogisticalRegression.FSharp/Program.fs
+++ LogisticalRegression.FSharp/Program.fs
@@ -1,7 +1,27 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System
+open System.IO
+open System.Linq
+open MathNet.Numerics.LinearAlgebra.Double
+open MathNet.Numerics.LinearAlgebra.Generic
+open MathNet.Numerics.LinearAlgebra.Generic.FSharpExtensions
+open MathNet.Numerics.Statistics
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    printfn "Loading data..."
+
+    let data = File.ReadLines "ex1data2.txt" 
+               |> Seq.map (fun s -> s.Split ',')
+               |> Array.ofSeq
+               |> Array.map (fun a -> Array.map Double.Parse a)
+    
+    // There has got to be a more elegant way to populate these from a CSV file in F#...
+    let sourceX = DenseMatrix.init data.Length (data.First().Length - 1) (fun i j -> data.[i].[j])
+    let y = DenseVector.init data.Length (fun i -> data.[i].Last())
+
+    // TODO:
+
+    printfn "Done!"
+    Console.ReadLine()
+
     0 // return an integer exit code
